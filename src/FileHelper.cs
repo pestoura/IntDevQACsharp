4a483fd49a5dd8718b1d3286921dde33e9cc51dd
Pedro Estoura <115967430+pestoura@users.noreply.github.com>
2024-07-09 16:03:58 +0100

    Update FileHelper.cs

--- src/FileHelper.cs
+++ src/FileHelper.cs
@@ -1,1 +1,30 @@
+using System.IO;
+
+public static class FileHelper
+{
+    public static void CopyFile(string sourcePath, string destPath)
+    {
+        if (!Directory.Exists(Path.GetDirectoryName(destPath)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(destPath));
+        }
+        File.Copy(sourcePath, destPath, true);
+    }
+
+    public static void DeleteFile(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+    }
+
+    public static void DeleteDirectory(string dirPath)
+    {
+        if (Directory.Exists(dirPath))
+        {
+            Directory.Delete(dirPath, true);
+        }
+    }
+}
 
